8f16798f12e525094a8bf0058856991d97fe4e90
edm00 <esniffdevices@outlook.com>
2025-02-17 15:35:43 +0530

    changes to headers of testdata

--- add_headers.py
+++ add_headers.py
@@ -1,12 +1,8 @@
 import os
 import pandas as pd
 
-# Define the folder path where your CSV files are located
-folder_path = './new_device_data'
-
-# Define the headers you want to add
-headers = ['Time', 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5',
-           'sensor6', 'sensor7', 'sensor8', 'humidity', 'temperature']
+folder_path = './testdata'
+headers = ['Time', 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6', 'sensor7', 'sensor8', 'humidity', 'temperature']
 
 # Loop through each file in the folder
 for filename in os.listdir(folder_path):
@@ -15,10 +11,21 @@ for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
 
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(file_path, header=None)  # Assuming no headers are present
+        df = pd.read_csv(file_path, header=None) 
+        
+        # Check for headers
+        if df.iloc[0].tolist() == headers:
+            print(f'Headers already exist in {filename}')
+            # df = df[df.apply(lambda row: row.tolist() != headers, axis=1)]
+            df.drop(index=1)
+            df.to_csv(file_path, index=False)
 
+            continue
+        
         # Add headers to the DataFrame
         df.columns = headers
+
+        # Remove any additional headers if they exist
 
         # Save the DataFrame back to the CSV file
         df.to_csv(file_path, index=False)
